5bdc29ca399394e078ddc62b8b576bf5c1ac52c2
willy <dermawanwilly9@gmail.com>
2024-06-09 15:30:56 +0700

    logo2

--- resources/views/pages/home/user.blade.php
+++ resources/views/pages/home/user.blade.php
@@ -63,7 +63,7 @@
                 <div
                     class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-gray-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
                     <div class="h-52 flex flex-col justify-center items-center bg-rose-500 rounded-t-xl">
-                        <img src="../storage/bukti_pembayaran/Logo.png" class="object-cover h-36 w-36 rounded-full">
+                        <img src="../storage/bukti_pembayaran/Logo2.png" class="object-cover h-36 w-36 rounded-full">
                     </div>
                     <div class="p-4 md:p-6">
                         <h3 class="text-xl font-semibold text-gray-800 dark:text-neutral-200 dark:hover:text-white">
