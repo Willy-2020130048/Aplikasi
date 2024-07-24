509fef8f9bf0c8d5fcd0b98e4259db5b4be92bd4
Willy <dermawanwilly9@gmail.com>
2024-07-24 11:13:47 +0700

    format change

--- resources/views/email/index.blade.php
+++ resources/views/email/index.blade.php
@@ -10,7 +10,7 @@
     <h3>{{ $data['acara']->nama_acara }}</h3>
     <h3>Nomor Pendaftaran PITNAS</h3>
     <h3>
-    {{$data['acara']->jenis_acara}}{{$data['acara']->workshop == "Audit Klinis Dialisis" ? "Audit" : ""}}{{$data['acara']->workshop == "Health Technology Dialisis" ? "Health" : ""}}{{$data['acara']->workshop == "CAPD" ? "CAPD" : ""}}-{{ $data['detail']->id}}</h3>
+    {{$data['acara']->jenis_acara == "Workshop" ? "WS" : "Sim"}}{{$data['acara']->workshop == "Audit Klinis Dialisis" ? "1AKD" : ""}}{{$data['acara']->workshop == "Health Technology Dialisis" ? "2HTD" : ""}}{{$data['acara']->workshop == "CAPD" ? "3CAPD" : ""}}{{ $data['detail']->id}}</h3>
     <h4>Nama Lengkap</h4>
     <p>{{ $data['partisipan']->nama_lengkap }}</p>
     <h4>Jenis Kelamin</h4>
